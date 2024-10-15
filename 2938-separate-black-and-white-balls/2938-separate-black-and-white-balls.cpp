c953027bc8f84576c68bcf6559f6c00facc27431
Bertrand Wijaya <117497109+BBertt@users.noreply.github.com>
2024-10-15 22:47:37 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    long long minimumSteps(string s) {
+        int lastIndex = s.size()-1;
+        int swaps = 0;
+
+        for (int i = s.size()-1; i >= 0; i--) {
+            if (s[i] == '1') {
+                swaps += lastIndex - i;
+                lastIndex--;
+            }
+        }
+
+        return swaps;
+    }
+};
