0b9100f547e0df8bc51a5a3d616919025843d6f5
Bertrand Wijaya <117497109+BBertt@users.noreply.github.com>
2024-11-04 10:38:08 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3163-string-compression-iii/3163-string-compression-iii.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    string compressedString(string word) {
+        if (word.empty()) return "";
+
+        string comp;
+        char c;
+        int z = 0;
+
+        for (int i = 0 ; i < word.length(); i++) {
+            if (i == 0) {
+                c = word[i];
+                z++;
+            }
+            else {
+                if (word[i] == c) {
+                    z++;
+                }
+                else if (c != '\0' && word[i] != c){
+                    comp += to_string(z);
+                    comp += c;
+                    c = word[i];
+                    z = 1;
+                }
+                else if (c == '\0') {
+                    c = word[i];
+                    z = 1;
+                }
+                if (z == 9) {
+                    comp += to_string(z);
+                    comp += c;
+                    z = 0;
+                    c = '\0';
+                }
+            }
+            if (i == word.length()-1) {
+                comp += to_string(z);
+                comp += c;
+            }
+        }
+        return comp;
+    }
+};
