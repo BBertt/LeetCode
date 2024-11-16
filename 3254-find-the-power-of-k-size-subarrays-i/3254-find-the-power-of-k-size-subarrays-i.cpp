13b6675bb174998abb3ff42e39fb666889cacddf
Bertrand Wijaya <117497109+BBertt@users.noreply.github.com>
2024-11-16 21:16:32 +0700

    Time: 19 ms (7.5%), Space: 33.6 MB (19.07%) - LeetHub

---
+++ 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> resultsArray(vector<int>& nums, int k) {
+        vector<int> results;
+        vector<int> temp;
+        int c = 0;
+        int z = 0;
+        bool isAsc = true;
+        for (int i = 0; i < nums.size(); i++) {
+            temp.push_back(nums[i]);
+            c++;
+
+            if (c > 1) {
+                if (temp[c-2] + 1 != temp[c-1]) {
+                    isAsc = false;
+                }
+            }
+
+            if (c == k) {
+                if (isAsc == true) {
+                    results.push_back(temp[c-1]);
+                }
+                else {
+                    results.push_back(-1);
+                }
+                c = 0;
+                isAsc = true;
+                z++;
+                i = z-1;
+                temp.clear();
+            }
+        }
+
+        return results;
+    }
+};
